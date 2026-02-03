9df1505aae2c801793838a483eb8bc3ede867921
Airlangga <airlanggakrishna@gmail.com>
2026-02-03 23:49:21 +0700

    chore: define entities

--- entity.go
+++ entity.go
@@ -1,8 +1,41 @@
 package main
 
+import "time"
+
 type User struct {
 	Id       int
 	Email    string
 	Password string
 	Role     string
+}
+
+type Product struct {
+	Id          int
+	Name        string
+	Description string
+	Price       int
+}
+
+type CartItem struct {
+	Id        int
+	UserId    int
+	ProductId int
+	UnitPrice int
+	Quantity  int
+}
+
+type OrderItem struct {
+	Id        int
+	OrderId   int
+	ProductId int
+	UnitPrice int
+	Quantity  int
+}
+
+type Order struct {
+	Id         int
+	UserId     int
+	TotalPrice int
+	CreatedAt  time.Time
+	Products   []Product
 }
